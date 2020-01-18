dd5ee71ba40e36b1eaab02a0846961355d0071be
Love Mercy <lovemercybiz@gmail.com>
2020-01-18 10:56:59 -0500

    Finished Static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,26 @@
 class StaticPagesController < ApplicationController
+  before_action :page_title
+  
   def home
+    @title
   end
 
   def help
+    @title
   end
+
+  def about
+    @title
+  end
+
+  def contact
+    @title
+  end
+  
+  private
+
+  def page_title
+    @title = self.action_name.capitalize + " | Sample App"
+  end
+
 end
